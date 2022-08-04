65d1d76cdfb3547e09defa412b3bdc1872052d88
Durgesh Upadhyay <udurgesh6@gmail.com>
2022-08-04 20:53:28 +0530

    axios api basics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
 import Child2 from "./Child2";
+import axios from "axios";
 
 const App = () => {
-  const [childDisable, setChildDisable] = useState(true);
-  const disableChild = () => {
-    setChildDisable((childDisable) => !childDisable);
+  const [products, setProducts] = useState([]);
+  const callProductsApi = () => {
+    axios
+      .get("https://fakestoreapi.com/products/1")
+      .then((response) => {
+        console.log(response.data);
+        setProducts(response.data);
+      })
+      .catch((err) => {
+        console.log("Some error");
+      })
+      .finally(() => {
+        console.log("Finally");
+      });
   };
   return (
     <div style={{ border: "1px solid blue" }}>
-      {childDisable ? <Child2 /> : <div>No Child</div>}
-      <button onClick={disableChild}>Disable/Enable</button>
+      <button onClick={callProductsApi}>Call API</button>
+      {products.map((product) => (
+        <div key={product.id}>
+          <p>Title - {product.title}</p>
+          <p>Price - {product.price}</p>
+        </div>
+      ))}
     </div>
   );
 };
